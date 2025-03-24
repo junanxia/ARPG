c434dd3bed0216d218ed602ac5271e20a20d9c6c
root <junan007@163.com>
2025-03-24 17:06:32 +0800

    P3 finished.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var input_vec = Vector2.ZERO
 	
 	input_vec.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -12,8 +12,8 @@ func _physics_process(delta: float) -> void:
 	input_vec = input_vec.normalized()
 	
 	if input_vec != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vec * MAX_SPEED, ACCELERATION) * delta
+		velocity = velocity.move_toward(input_vec * MAX_SPEED, ACCELERATION)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION) * delta
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
-	move_and_collide(velocity)
+	move_and_slide()

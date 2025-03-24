d604156bf6bded8ea78778d64ab9d955dc983154
root <junan007@163.com>
2025-03-24 11:16:52 +0800

    资产导入

---
+++ Scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 100
+const FRICTION = 500
+
+func _physics_process(delta: float) -> void:
+	var input_vec = Vector2.ZERO
+	
+	input_vec.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vec.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vec = input_vec.normalized()
+	
+	if input_vec != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vec * MAX_SPEED, ACCELERATION) * delta
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION) * delta
+	
+	move_and_collide(velocity)

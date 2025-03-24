0328b66cfcb4f06eff89400a6baa9833e95c0523
junan <junan007@163.com>
2025-03-24 22:21:22 +0800

    update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,10 @@ const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
+
 func _physics_process(_delta: float) -> void:
 	var input_vec = Vector2.ZERO
 	
@@ -12,8 +16,13 @@ func _physics_process(_delta: float) -> void:
 	input_vec = input_vec.normalized()
 	
 	if input_vec != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vec)
+		animation_tree.set("parameters/Run/blend_position", input_vec)
+		animation_state.travel("Run")
+		
 		velocity = velocity.move_toward(input_vec * MAX_SPEED, ACCELERATION)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
 	move_and_slide()
